e3b0242480b8bf3fdef4b6e51a07e26bcc4ad4e2
DicksonKwong <Dickson8002@gmail.com>
2026-03-10 02:24:38 -0400

    Update connect.py

--- ingest3rs/Pinecone/connect/connect.py
+++ ingest3rs/Pinecone/connect/connect.py
@@ -1,13 +1,18 @@
-from pinecone.grpc import PineconeGRPC, GRPCClientConfig
-from pinecone import ServerlessSpec
+from pinecone.grpc import PineconeGRPC
 import requests
-
-
 # Initialize a client.
 # API key is required, but the value does not matter.
 # Host and port of the Pinecone Local instance
 # is required when starting without indexes. 
+
 pc = PineconeGRPC(
-    api_key="pclocal", 
-    host="http://localhost:5080" 
+    api_key="pclocal",
+    host="http://localhost:5081"
 )
+
+try:
+    indexes = pc.list_indexes()
+    print("Pinecone is alive:", indexes)
+except Exception as e:
+    print("Pinecone not responding:", e)
+
